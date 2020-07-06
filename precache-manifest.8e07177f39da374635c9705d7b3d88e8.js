cf074c6d96c9b22a0265d8347b2ea49dc0439184
Amay Kataria <amkatari@microsoft.com>
2020-07-05 19:23:37 -0500

    Updates

---
+++ precache-manifest.8e07177f39da374635c9705d7b3d88e8.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d45c551eb288d199764d380ee6459a7c",
+    "url": "/biographyofaportrait/index.html"
+  },
+  {
+    "revision": "a1f2bbcd5ce368f74b8d",
+    "url": "/biographyofaportrait/static/css/main.f5425955.chunk.css"
+  },
+  {
+    "revision": "0649440a4f1e714b146c",
+    "url": "/biographyofaportrait/static/js/2.928dd14c.chunk.js"
+  },
+  {
+    "revision": "7394186c78c74bd60264b20e3b2b8afb",
+    "url": "/biographyofaportrait/static/js/2.928dd14c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1f2bbcd5ce368f74b8d",
+    "url": "/biographyofaportrait/static/js/main.6f941159.chunk.js"
+  },
+  {
+    "revision": "e797ef941c42eb947915",
+    "url": "/biographyofaportrait/static/js/runtime-main.2df0d599.js"
+  },
+  {
+    "revision": "0c6cd72756c6ef3bcaf0337ad7036b56",
+    "url": "/biographyofaportrait/static/media/bebasneue-webfont.0c6cd727.woff"
+  },
+  {
+    "revision": "8a018e1125b162bff6b5fbd9179bcce6",
+    "url": "/biographyofaportrait/static/media/bebasneue-webfont.8a018e11.woff2"
+  },
+  {
+    "revision": "369e87dabfb0b07ef9f813402a24efb2",
+    "url": "/biographyofaportrait/static/media/bilbo-regular-webfont.369e87da.woff2"
+  },
+  {
+    "revision": "b3d46d4c29195ce4b4cf5725b3f79b9a",
+    "url": "/biographyofaportrait/static/media/bilbo-regular-webfont.b3d46d4c.woff"
+  },
+  {
+    "revision": "4f7724ee0afd53a845c84237aaef47c1",
+    "url": "/biographyofaportrait/static/media/merchantcopydoublesize-je7r-webfont.4f7724ee.woff2"
+  },
+  {
+    "revision": "883ed9a971d52a798c6682f227e076a4",
+    "url": "/biographyofaportrait/static/media/merchantcopydoublesize-je7r-webfont.883ed9a9.woff"
+  },
+  {
+    "revision": "87b33e2de3819ca5260319ef546738c9",
+    "url": "/biographyofaportrait/static/media/opensans-light-webfont.87b33e2d.woff2"
+  },
+  {
+    "revision": "9ad03de81bbf6cd48ffc44cf2aa4d707",
+    "url": "/biographyofaportrait/static/media/opensans-light-webfont.9ad03de8.woff"
+  },
+  {
+    "revision": "65a61f9230a85b44df43968f80a522e5",
+    "url": "/biographyofaportrait/static/media/opensans-regular-webfont.65a61f92.woff"
+  },
+  {
+    "revision": "d407a58546593548175615d7621183ef",
+    "url": "/biographyofaportrait/static/media/opensans-regular-webfont.d407a585.woff2"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.07ca643d95f151c7583052b4441beb21.js"
+  "/biographyofaportrait/precache-manifest.8e07177f39da374635c9705d7b3d88e8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/biographyofaportrait/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
