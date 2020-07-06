5553c69282955d8d4119ffb967b8d3823850b757
Amay Kataria <amkatari@microsoft.com>
2020-07-05 19:21:05 -0500

    Updates

---
+++ precache-manifest.07ca643d95f151c7583052b4441beb21.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "818d969e93fa0d7550d6b1ce78a467f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "286d66e9b05233c18e35",
+    "url": "/static/css/main.60155e5f.chunk.css"
+  },
+  {
+    "revision": "0649440a4f1e714b146c",
+    "url": "/static/js/2.928dd14c.chunk.js"
+  },
+  {
+    "revision": "7394186c78c74bd60264b20e3b2b8afb",
+    "url": "/static/js/2.928dd14c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "286d66e9b05233c18e35",
+    "url": "/static/js/main.822b0081.chunk.js"
+  },
+  {
+    "revision": "cc8c22a03749e0ab42de",
+    "url": "/static/js/runtime-main.5f052b60.js"
+  },
+  {
+    "revision": "0c6cd72756c6ef3bcaf0337ad7036b56",
+    "url": "/static/media/bebasneue-webfont.0c6cd727.woff"
+  },
+  {
+    "revision": "8a018e1125b162bff6b5fbd9179bcce6",
+    "url": "/static/media/bebasneue-webfont.8a018e11.woff2"
+  },
+  {
+    "revision": "369e87dabfb0b07ef9f813402a24efb2",
+    "url": "/static/media/bilbo-regular-webfont.369e87da.woff2"
+  },
+  {
+    "revision": "b3d46d4c29195ce4b4cf5725b3f79b9a",
+    "url": "/static/media/bilbo-regular-webfont.b3d46d4c.woff"
+  },
+  {
+    "revision": "4f7724ee0afd53a845c84237aaef47c1",
+    "url": "/static/media/merchantcopydoublesize-je7r-webfont.4f7724ee.woff2"
+  },
+  {
+    "revision": "883ed9a971d52a798c6682f227e076a4",
+    "url": "/static/media/merchantcopydoublesize-je7r-webfont.883ed9a9.woff"
+  },
+  {
+    "revision": "87b33e2de3819ca5260319ef546738c9",
+    "url": "/static/media/opensans-light-webfont.87b33e2d.woff2"
+  },
+  {
+    "revision": "9ad03de81bbf6cd48ffc44cf2aa4d707",
+    "url": "/static/media/opensans-light-webfont.9ad03de8.woff"
+  },
+  {
+    "revision": "65a61f9230a85b44df43968f80a522e5",
+    "url": "/static/media/opensans-regular-webfont.65a61f92.woff"
+  },
+  {
+    "revision": "d407a58546593548175615d7621183ef",
+    "url": "/static/media/opensans-regular-webfont.d407a585.woff2"
+  }
+]);
