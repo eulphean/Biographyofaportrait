a4518847f7f4fe6e19f65c4a26fb274ea0eabf0e
Amay Kataria <amaykataria@amays-mbp.attlocal.net>
2020-07-20 01:33:52 -0500

    User heights

--- WebClient/src/components/Today.js
+++ WebClient/src/components/Today.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 import Banner from './Banner.js'
 import CustomButton from './CustomButton.js'
 import { ReactComponent as User } from '../svg/banda.svg'
-import { color } from './CommonStyles.js'
+import { padding, color } from './CommonStyles.js'
 
 const RadiumLink = Radium(Link)
 
@@ -13,13 +13,43 @@ const styles = {
   container: {
     position: 'relative',
     display: 'flex',
-    justifyContent: 'center'
+    justifyContent: 'center',
+
+    '@media (min-width: 768px)': {  
+      marginLeft: padding.enourmous,
+      marginRight: padding.enourmous,
+    },
   },
 
   user: {
     position: 'fixed',
-    width: '350px',
-    bottom: '-3%'
+    width: '120vw',
+    bottom: '-5%',
+    overflow:'none',
+
+    '@media (min-width: 450px)': {  
+      // no change.
+      width: '100vw',
+      bottom: '-15%'
+    },
+
+    '@media (min-width: 600px)': {  
+    },
+
+    '@media (min-width: 700px)': {  
+      width: '80vw',
+      bottom: '-10%'
+    },
+
+    '@media (min-width: 900px)': {  
+      width: '70vw',
+      bottom: '-20%'
+    },
+
+    '@media (min-width: 1200px)': {  
+      width: '60vw',
+      bottom: '-25%'
+    }
   },
 
   icon: {
