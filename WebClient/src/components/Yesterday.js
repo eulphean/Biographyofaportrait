74da90dfb6d3b8a0d1fa8fe672ef92330f869311
Amay Kataria <amaykataria@amays-mbp.attlocal.net>
2020-07-26 21:22:33 -0500

    Center the folder everywhere

--- WebClient/src/components/Yesterday.js
+++ WebClient/src/components/Yesterday.js
@@ -10,7 +10,9 @@ const styles = {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    width: '100vw',
+    height: '100vh'
   }
 };
 
