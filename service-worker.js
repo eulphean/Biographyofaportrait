16f86bb65329156c817cdaa3cbba0e74aa1be337
Amay Kataria <amkatari@microsoft.com>
2020-08-02 22:15:00 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/biographyofaportrait/precache-manifest.7c4e3502e44fefbf537f2eff454e28bc.js"
+  "/biographyofaportrait/precache-manifest.6f2a2c8d79b8faa0a7a084e4cdd29572.js"
 );
 
 self.addEventListener('message', (event) => {
